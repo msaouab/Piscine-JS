e2bf4abffbf6c1f9626b6ba2ea2112374d0155f3
msaouab <saouab.mohammed@gmail.com>
2023-01-23 13:08:41 +0100

    js00/ex02 Not Complet

--- JS/js00/ex03/dynamicType.js
+++ JS/js00/ex03/dynamicType.js
@@ -1,17 +1,37 @@
 
+let myFunc = num => Number(num);
+
 const dynamicType = {
-	value: null,
+	constructor () {
+		value: null;
+	},
 	put: function (num) {
-		if (num === null)
+		if (this.value == null)
 			this.value = num;
 		else
 			console.log("The put function should only be used once before the printType");
 	},
-	change: function () {
-		if (this.value === "string")
-			
+	change: function (type) {
+		if (type === "String")
+			this.value = this.value.toString();
+		else if (type === "Number")
+			this.value = Number(this.value);
+		else if (type === "Object")
+			this.value = JSON.parse(this.value);
+		else if (type === "Array") {
+			console.log(type);
+			this.value = Array.from(String(this.value), myFunc);
+		}
+		else
+			console.log("Invalid type provided for change function");
 	},
 	printType: function () {
-		console.log(`The value is of type ${typeof this.value}`);
+		console.log(`The value is of type ${typeof this.value}, ${this.value}`);
 	}
 }
+
+const type = dynamicType;
+
+type.put("30");
+type.change("Array");
+type.printType();
