508ba6b00203e11a8edf0bc3c6edfd3ba5804b3c
msaouab <saouab.mohammed@gmail.com>
2023-01-25 17:15:47 +0100

    js01/ex04 done.

--- JS/js01/ex04/script.js
+++ JS/js01/ex04/script.js
@@ -1,10 +1,12 @@
 
-let oldScroll = window.scrollY;
+let lastScrollTop = 0
 
-window.onscroll = function (event) {
-	let newScroll = window.scrollY;
-	if (oldScroll < newScroll)
-	console.log("Up");
-	if (oldScroll > newScroll)
-		console.log("Down");
-}
+window.addEventListener("scroll", function(){
+   var st = window.pageYOffset || document.documentElement.scrollTop;
+   if (st > lastScrollTop){
+       console.log('Scrolling down...', st);
+   } else {
+      console.log('Scrolling up...', st);
+   }
+   lastScrollTop = st <= 0 ? 0 : st; 
+}, false);
